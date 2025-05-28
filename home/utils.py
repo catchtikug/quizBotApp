26c6eae4489ebffcfe22647fb99a0c74f308264e
catchtikug <catchtik@gmail.com>
2025-05-28 13:08:37 +0300

    pushing the work

--- home/utils.py
+++ home/utils.py
@@ -44,8 +44,8 @@ def extract_mc_questions(pdf_file):
 # pdf_file = "28_Hosea_MCQuestions.pdf"
 # output_csv = "hosea_questions.csv"
 
-pdf_file = "02_Mark_MC_Questions.pdf"
-output_csv = "mark_questions.csv"
+pdf_file = "03_LeviticusMCQuestions.pdf"
+output_csv = "leviticus_questions.csv"
 
 print(f"📄 Reading: {pdf_file}")
 data = extract_mc_questions(pdf_file)
